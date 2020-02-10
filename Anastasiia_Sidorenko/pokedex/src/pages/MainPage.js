00f877d8b39328eacdbfe04067e1f7301af82185
Anastasia Yasyutina <a.yasutina@gmail.com>
2020-02-10 21:22:25 +0300

    fetches url

--- Anastasiia_Sidorenko/pokedex/src/pages/MainPage.js
+++ Anastasiia_Sidorenko/pokedex/src/pages/MainPage.js
@@ -1,61 +1,34 @@
 import React from 'react';
 import Pokemon from '../components/pokemon/Pokemon';
 
-const MainPage = () => {
-    const pokemons = [
-        {
-            "name": "bulbasaur",
-            "id": 1
-        },
-        {
-            "name": "ivysaur",
-            "id": 2
-        },
-        {
-            "name": "venusaur",
-            "id": 3
-        },
-        {
-            "name": "charmander",
-            "id": 4
-        },
-        {
-            "name": "charmeleon",
-            "id": 5
-        },
-        {
-            "name": "charizard",
-            "id": 6
-        },
-        {
-            "name": "squirtle",
-            "id": 7
-        },
-        {
-            "name": "wartortle",
-            "id": 8
-        },
-        {
-            "name": "blastoise",
-            "id": 9
-        },
-        {
-            "name": "caterpie",
-            "id": 10
-        }
-    ];
+class MainPage extends React.Component {
+    constructor(props) {
+        super(props);
 
-    const handleClick = () => {
-       alert('it works');
+        this.state = {
+            pokemons: []
+        }
     }
-    
-    return (
-        <div className="container">
-            <div className="row row-cols-1 row-cols-md-4">
-                {pokemons.map(pokemon => <Pokemon handleClick={handleClick} key={pokemon.id} name={pokemon.name} id={pokemon.id} showButton={true} />)}
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+          .then(response => response.json())
+          .then(json => this.setState({ pokemons: json }));
+      }
+
+    handleClick = () => {
+        alert('it works');
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <div className="row row-cols-1 row-cols-md-4">
+                    {this.state.pokemons.map(pokemon => <Pokemon handleClick={this.handleClick} key={pokemon.id} name={pokemon.name} id={pokemon.id} showButton={true} />)}
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
 
 export default MainPage;
